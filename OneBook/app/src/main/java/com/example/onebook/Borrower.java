87c7ac31852500829d87607c7691ff768990aa4c
dmcrorie <dmcrorie@ualberta.ca>
2019-02-21 20:24:35 -0700

    Added the User, Borrower, and Owner dummy classes

---
+++ OneBook/app/src/main/java/com/example/onebook/Borrower.java
@@ -0,0 +1,15 @@
+package com.example.onebook;
+
+public class Borrower extends User {
+
+    public void requestBook(int ISBN, int userID) {
+
+
+        return;
+    }
+
+    public void returnBook(int ISBN, int userID) {
+
+        return;
+    }
+}
