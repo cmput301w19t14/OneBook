3875150d50bf85884affefb1b95786712a8a11b8
ana16boo@gmail.com <42515487+Ana16boo@users.noreply.github.com>
2019-02-24 00:01:11 -0700

    Added Request Java class and Shell for future test cases

---
+++ OneBook/app/src/test/java/com/example/onebook/RequestTest.java
@@ -0,0 +1,33 @@
+package com.example.onebook;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class RequestTest {
+
+    @Test
+    public void testSendNotification() {
+
+    }
+
+    @Test
+    public void testSetLocation() {
+
+    }
+
+    @Test
+    public void testSetOwner() {
+
+    }
+
+    @Test
+    public void testSetBorrower() {
+
+    }
+
+    @Test
+    public void testSetBook() {
+
+    }
+}
