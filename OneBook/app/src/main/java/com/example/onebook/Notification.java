9077919cc4a2d459e4b81b7eae6e3483c6bae210
Natalie Hervieux <nhervieu@ualberta.ca>
2019-02-25 13:30:55 -0700

    notification class created

---
+++ OneBook/app/src/main/java/com/example/onebook/Notification.java
@@ -0,0 +1,50 @@
+package com.example.onebook;
+
+public class Notification{
+
+    private Boolean read;
+    private String notification;
+    private Integer senderID;
+    private Integer recipientID;
+
+
+    public Boolean getRead() {
+        return read;
+    }
+    public void setRead(Boolean read) {
+        this.read = read;
+    }
+
+    public String getNotification() {
+        return notification;
+    }
+    public void setNotification(String notification) {
+        this.notification = notification;
+    }
+
+    public Integer getSenderID() {
+        return senderID;
+    }
+    public void setSenderID(Integer senderID) {
+        this.senderID = senderID;
+    }
+
+    public Integer getRecipientID() {
+        return recipientID;
+    }
+    public void setRecipientID(Integer recipientID) {
+        this.recipientID = recipientID;
+    }
+
+    public Boolean createNotification(Integer notificationType, Integer senderID,  Integer recipientID, String notification){
+        return Boolean.TRUE;
+    }
+
+    public Boolean deleteNotification(Integer notificationType, Integer senderID,  Integer recipientID, String notification){
+        return Boolean.TRUE;
+    }
+
+    public Boolean sendNotification(Integer notificationType, Integer senderID,  Integer recipientID, String notification){
+        return Boolean.TRUE;
+    }
+}
