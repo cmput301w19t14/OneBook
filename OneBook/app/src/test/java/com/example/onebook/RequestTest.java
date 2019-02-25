e526d598299c4d4afb5dfacfd847e53f379cfdd2
ana16boo@gmail.com <42515487+Ana16boo@users.noreply.github.com>
2019-02-25 13:44:34 -0700

    intermediate changes

--- OneBook/app/src/test/java/com/example/onebook/RequestTest.java
+++ OneBook/app/src/test/java/com/example/onebook/RequestTest.java
@@ -17,7 +17,7 @@ public class RequestTest {
     private Book book = new Book(1,"Narnia","narnia guy","narnia book",
             req,owner,borrow, place, "available");
     private Borrower borrower = new Borrower("sam", "sam12", "sam@gmail.com", 121544789, 11111);
-    private Location location = new Location(12.11, 12.33, "Paris", "1124 Street", "France", "T6G 2T7");
+    private Location location = new Location("test", "Paris", "1124 Street", "France", "T6G 2T7");
     private Request request = new Request(owner, borrower, book);
 
     @Test
@@ -27,27 +27,30 @@ public class RequestTest {
 
     @Test
     public void testSetLocation() {
-
-        assertEquals(request.getLocation(), request.setLocation(location));
+        request.setLocation(location);
+        assertEquals(location, request.getLocation());
     }
 
     @Test
     public void testSetOwner() {
         Owner owner1 = new Owner("mary", "hunter2", "john2@gmail.com",
                 7804054451L, 110566);
-        assertEquals(request.getOwner(), request.setOwner(owner1));
+        request.setOwner(owner1);
+        assertEquals(owner1, request.getOwner());
     }
 
     @Test
     public void testSetBorrower() {
-        private Borrower borrower1 = new Borrower("tom", "sam12", "sam@gmail.com", 121544789, 113411);
-        assertEquals(request.getBorrower(), request.setBorrower(borrower1));
+        Borrower borrower1 = new Borrower("tom", "sam12", "sam@gmail.com", 121544789, 113411);
+        request.setBorrower(borrower1);
+        assertEquals(borrower1, request.getBorrower());
     }
 
     @Test
     public void testSetBook() {
         Book book1 = new Book(123,"Another Narnia","narnia guy","narnia book",
                 req,owner,borrow, place, "available");
-        assertEquals(request.getBook(), request.setBook(book1));
+        request.setBook(book1);
+        assertEquals(book1, request.getBook());
     }
 }
