5e0f1e52a533253ad813df5dfb4eb1b0ee28c494
dmcrorieGIT <34727471+dmcrorieGIT@users.noreply.github.com>
2019-02-25 10:07:51 -0700

    Update fix
    ignore.

--- OneBook/app/src/main/java/com/example/onebook/Owner.java
+++ OneBook/app/src/main/java/com/example/onebook/Owner.java
@@ -1,6 +1,12 @@
 package com.example.onebook;
 
 public class Owner extends User {
+    
+    Owner(String username, String password, String email, long phone, int userID) {
+
+        super(username, password, email, phone, userID);
+
+    } 
 
     public boolean lendBook(Book book, int userID) {
         //code to be added
