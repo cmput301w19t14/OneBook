2f40be41f4c034376cf00824474549ed0693b276
Dimitri Trofimuk <dimitri.trofimuk@ualberta.ca>
2019-03-02 09:47:43 -0700

    reinit android studio project

---
+++ OneBook-new/app/src/main/java/ca/ualberta/c301w19t14/onebook/HomeActivity.java
@@ -0,0 +1,13 @@
+package ca.ualberta.c301w19t14.onebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
