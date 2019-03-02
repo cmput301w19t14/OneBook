48fa97779f535a5db43a9f36105b9730d4c3d77f
Dimitri Trofimuk <dimitri.trofimuk@ualberta.ca>
2019-03-02 10:39:25 -0700

    Navigation init
    also added a few icons for the various pages

--- OneBook-new/app/src/main/java/ca/ualberta/c301w19t14/onebook/MainActivity.java
+++ OneBook-new/app/src/main/java/ca/ualberta/c301w19t14/onebook/MainActivity.java
@@ -67,9 +67,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+         //   return true;
+        //}
 
         return super.onOptionsItemSelected(item);
     }
@@ -80,19 +80,10 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        //if (id == R.id.nav_camera) {
+        //    // Handle the camera action
+       // } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
