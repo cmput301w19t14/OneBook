126b5490c23a442fb12ad46e24ffe63d056110ee
dmcrorie <dmcrorie@ualberta.ca>
2019-03-03 23:11:42 -0700

    Rough version of Registering new user

--- OneBook/app/src/main/java/com/example/onebook/UserRegisterActivity.java
+++ OneBook/app/src/main/java/com/example/onebook/UserRegisterActivity.java
@@ -6,9 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class UserRegisterActivity extends AppCompatActivity {
 
     private User newuser;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
     public Button registerbutton;
     public EditText useredit;
     public EditText nameedit;
@@ -20,9 +26,13 @@ public class UserRegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         registerbutton = (Button) findViewById(R.id.UR_loginbutton);
-        useredit = (EditText) findViewById(R.id.UL_useredit);
+        useredit = (EditText) findViewById(R.id.UR_useredit);
         nameedit = (EditText) findViewById(R.id.UR_editname);
         passedit = (EditText) findViewById(R.id.UR_passedit);
+
+        //assign firebase objects
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
 
         registerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,8 +50,60 @@ public class UserRegisterActivity extends AppCompatActivity {
         name = nameedit .getText().toString();
         pass = passedit.getText().toString();
 
+        //replace illegal characters
+        email = removeAt(email);
+        email = removeDot(email);
+
+        //add user to database
+        myRef.child("Users").child(email).child("Password").setValue(pass);
+        myRef.child("Users").child(email).child("Email").setValue(email);
+        myRef.child("Users").child(email).child("Name").setValue(name);
+        //TEMP: dummy variables for phone and userID, resolve this later
+        myRef.child("Users").child(email).child("Phone").setValue(1000000000L);
+        myRef.child("Users").child(email).child("UserID").setValue(99999L);
+
         newuser = new Borrower(name, pass, email);
 
         return true;
+    }
+
+    public static String removeAt(String email){
+        char[] emailarray = email.toCharArray();
+        for (int i = 0; i < emailarray.length; i++){
+            if (emailarray[i] == '@')
+                emailarray[i] = '*';
+        }
+        email = String.valueOf(emailarray);
+        return email;
+    }
+
+    public static String putAt(String email){
+        char[] emailarray = email.toCharArray();
+        for (int i = 0; i < emailarray.length; i++){
+            if (emailarray[i] == '*')
+                emailarray[i] = '@';
+        }
+        email = String.valueOf(emailarray);
+        return email;
+    }
+
+    public static String removeDot(String email){
+        char[] emailarray = email.toCharArray();
+        for (int i = 0; i < emailarray.length; i++){
+            if (emailarray[i] == '.')
+                emailarray[i] = '_';
+        }
+        email = String.valueOf(emailarray);
+        return email;
+    }
+
+    public static String putDot(String email){
+        char[] emailarray = email.toCharArray();
+        for (int i = 0; i < emailarray.length; i++){
+            if (emailarray[i] == '_')
+                emailarray[i] = '.';
+        }
+        email = String.valueOf(emailarray);
+        return email;
     }
 }
