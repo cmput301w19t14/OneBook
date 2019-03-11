7c6d4d57dea5f111d051536b362e076fa8b67607
JandaileD <43227764+JandaileD@users.noreply.github.com>
2019-03-11 15:48:10 -0600

    Added JavaDocs
    for:
    
    Notification
    Notification Fragment
    Request

--- OneBook-new/app/src/main/java/ca/ualberta/c301w19t14/onebook/Notification.java
+++ OneBook-new/app/src/main/java/ca/ualberta/c301w19t14/onebook/Notification.java
@@ -1,5 +1,7 @@
 package ca.ualberta.c301w19t14.onebook;
 
+/**This class will be used in part 5
+ * @author CMPUT 301 Team 14*/
 public class Notification{
 
     private Boolean read;
@@ -7,47 +9,103 @@ public class Notification{
     private String senderID;
     private String recipientID;
 
-
+    /**
+     * getter for the read status
+     * @return read
+     */
     public Boolean getRead() {
         return read;
     }
+
+    /**
+     * setter for the set read
+     * @param read
+     */
     public void setRead(Boolean read) {
         this.read = read;
     }
 
+    /**
+     * getter for notifications
+     * @return notification
+     */
     public String getNotification() {
         return notification;
     }
+
+    /**
+     * setter for notification
+     * @param notification
+     */
     public void setNotification(String notification) {
         this.notification = notification;
     }
 
+    /**
+     * getter for the senderID
+     * @return senderID
+     */
     public String getSenderID() {
         return senderID;
     }
+
+    /**
+     * setter for the senderID
+     * @param senderID
+     */
     public void setSenderID(String senderID) {
         this.senderID = senderID;
     }
 
+    /**
+     * getter for the recipient ID
+     * @return recipientID
+     */
     public String getRecipientID() {
         return recipientID;
     }
+
+    /**
+     * setter for the recipient ID
+     * @param recipientID
+     */
     public void setRecipientID(String recipientID) {
         this.recipientID = recipientID;
     }
 
-    public Boolean createNotification(Integer notificationType, String senderID,
-                                      String recipientID, String notification){
+    /**
+     *
+     * @param notificationType
+     * @param senderID
+     * @param recipientID
+     * @param notification
+     * @return boolean
+     */
+    public Boolean createNotification(Integer notificationType, Integer senderID,  Integer recipientID, String notification){
         return Boolean.TRUE;
     }
 
-    public Boolean deleteNotification(Integer notificationType, String senderID,
-                                      String recipientID, String notification){
+    /**
+     *
+     * @param notificationType
+     * @param senderID
+     * @param recipientID
+     * @param notification
+     * @return boolean
+     */
+    public Boolean deleteNotification(Integer notificationType, Integer senderID,  Integer recipientID, String notification){
         return Boolean.TRUE;
     }
 
-    public Boolean sendNotification(Integer notificationType, String senderID,
-                                    String recipientID, String notification){
+    /**
+     *
+     * @param notificationType
+     * @param senderID
+     * @param recipientID
+     * @param notification
+     * @return boolean
+     */
+    public Boolean sendNotification(Integer notificationType, Integer senderID,  Integer recipientID, String notification){
         return Boolean.TRUE;
     }
 }
