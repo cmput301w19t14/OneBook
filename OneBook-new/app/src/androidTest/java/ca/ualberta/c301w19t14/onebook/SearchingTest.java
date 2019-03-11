10e2f418c2de606c267ddcbc4aab0185002d2101
oran <oran@ualberta.ca>
2019-03-11 15:56:23 -0600

    searching test

--- OneBook-new/app/src/androidTest/java/ca/ualberta/c301w19t14/onebook/SearchingTest.java
+++ OneBook-new/app/src/androidTest/java/ca/ualberta/c301w19t14/onebook/SearchingTest.java
@@ -1,27 +1,30 @@
 package ca.ualberta.c301w19t14.onebook;
 
-import android.support.test.espresso.contrib.RecyclerViewActions;
+//import androidx.test.espresso.contrib.RecyclerViewActions;
+//import android.support.v7.widget.RecyclerView;
+
+//import androidx.test.espresso.contrib.RecyclerViewActions;
 
 import org.junit.Rule;
+import org.junit.Test;
 
 import java.util.Map;
 
-import androidx.test.espresso.matcher.ViewMatchers;
+//import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+//import static androidx.test.espresso.Espresso.onData;
+//import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+//import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.Espresso.onView;
 //import static android.test.espresso..contrib;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
+//import static androidx.test.espresso.assertion.ViewAssertions.matches;
 //import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
+
 
 public class SearchingTest {
 
@@ -30,12 +33,13 @@ public class SearchingTest {
     @Rule
     public ActivityTestRule<SearchingActivity> activityRule =
             new ActivityTestRule<SearchingActivity>(SearchingActivity.class);
-
+    @Test
     public void SearchingTest()
     {
         onView(withId(R.id.search)).perform(typeText("te"));
         onView(withId(R.id.SearchButton)).perform(click());
-        onView(withId(R.id.SearchRecycler)).check(matches(hasDescendant(withText("Some content"))));
+        //onView(withId(R.id.SearchRecycler)).perform(RecyclerViewActions.<VH>actionOnItemAtPosition(ITEM_BELOW_THE_));
+                //.check(matches(hasDescendant(withText("Test2"))));
 
 
 
