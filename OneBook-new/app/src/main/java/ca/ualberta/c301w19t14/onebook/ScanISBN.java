468449b4a1eaad6fc009db9d8c41197ae4009718
ana16boo@gmail.com <42515487+Ana16boo@users.noreply.github.com>
2019-03-17 16:08:11 -0600

    Barcode scan works

--- OneBook-new/app/src/main/java/ca/ualberta/c301w19t14/onebook/ScanISBN.java
+++ OneBook-new/app/src/main/java/ca/ualberta/c301w19t14/onebook/ScanISBN.java
@@ -1,13 +1,53 @@
 package ca.ualberta.c301w19t14.onebook;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class ScanISBN extends AppCompatActivity {
-
+    TextView barcodeResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_scan_isbn);
+        barcodeResult = (EditText) findViewById(R.id.enterIsbnManual);
+
+        Button scanButton =  findViewById(R.id.scanButton);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScanISBN.this, CameraActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==0) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra("barcode");
+                    barcodeResult.setText(barcode.displayValue);
+                }
+                else {
+                    barcodeResult.setHint("No barcode found. Please enter manually");
+                }
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
