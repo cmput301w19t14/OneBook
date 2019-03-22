ef43973d460bdce2344018e928f197c889e243a8
dmcrorie <dmcrorie@ualberta.ca>
2019-03-22 14:44:31 -0600

    fix for clicking item in borrowed books

--- OneBook-new/app/src/main/java/ca/ualberta/c301w19t14/onebook/BookAdapter.java
+++ OneBook-new/app/src/main/java/ca/ualberta/c301w19t14/onebook/BookAdapter.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -120,6 +121,11 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder
                 @Override public void onClick(View v){
                     Bundle bundle = new Bundle();
                     bundle.putString("id", book.getId());
+
+                    //DEBUG - remove later
+                    Toast toast = Toast
+                            .makeText(mContext, "Book ID: " + book.getTitle(), Toast.LENGTH_SHORT);
+                    toast.show();
 
                     //if the user clicks on a book they own, they will get a view page that allows edits
                     //if the user clicks on a book they do not own, they will get a view page that doesn't allow edits
