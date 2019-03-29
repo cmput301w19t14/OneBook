af29ae8aa2929618b149552101b7d1da33ab4c3c
nhervieu <nhervieu@ualberta.ca>
2019-03-28 21:43:56 -0600

    waitlist

--- OneBook-new/app/src/main/java/ca/ualberta/c301w19t14/onebook/models/Request.java
+++ OneBook-new/app/src/main/java/ca/ualberta/c301w19t14/onebook/models/Request.java
@@ -1,5 +1,7 @@
 package ca.ualberta.c301w19t14.onebook.models;
 
+import android.util.Log;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
